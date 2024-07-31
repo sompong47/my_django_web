36790152be4c68535f0695eb52a39affd88b3d7d
dream <deemgamer123@gmail.com>
2024-07-31 09:10:38 +0700

    config verecel

---
+++ webpage/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("", views.indexPage, name="index"),
+    path("about/", views.aboutUs, name="about"),
+    path("contact/", views.contactUs),
+]
